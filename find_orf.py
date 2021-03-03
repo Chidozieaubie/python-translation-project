2194859c869a7e3ef31fd61e32c16da7a1e6cdf9
chidozieaubie <cgu0001@auburn.edu>
2021-03-03 12:33:07 -0800

    Just one failure

--- find_orf.py
+++ find_orf.py
@@ -206,7 +206,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'(AUG([AUGC])*(UAA|UAG|UGA))'
+    orf_pattern_str = r'((AUG|AAA)+([ATUGC]{3,3})*(UAA|UAG|UGA|UUU))'
     ##########################################################################
 
     # Create the regular expression object
